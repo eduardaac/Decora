e26bc6ee2b85480af7c4dab90c77348ac1345849
Eduarda Araujo <duda2019h@gmail,com>
2023-06-09 16:19:12 -0300

    Logo inserida e pequenos ajustes feitos

--- projeto-ic/src/components/pages/cadastro/CadastroAD.js
+++ projeto-ic/src/components/pages/cadastro/CadastroAD.js
@@ -8,7 +8,7 @@ function CadastroaAD() {
   return (
     <dev className={styles.container}>
       <dev className={styles.loginAside}>
-        <img src={logo} style={{ height: "300px", width: '100%' }}></img>
+        <img src={logo} style={{ height: '55%', width: '90%' }}></img>
         <h1>Um sistema de recomendações de <span className={styles.cor}>dec
         </span>isões de pr<span className={styles.cor}>o</span>jeto arquitetu<span className= {styles.cor}>ra</span>l</h1>
       </dev>
