c60d3d80d02c21a7c127142b566bfd620f743863
Eduarda Araujo <duda2019h@gmail,com>
2023-06-09 16:17:54 -0300

    Logo permanente e pequenos ajustes feitos.

--- projeto-ic/src/components/pages/login/Login.js
+++ projeto-ic/src/components/pages/login/Login.js
@@ -8,9 +8,9 @@ function Login() {
   return (
     <dev className={styles.container}>
       <dev className={styles.loginAside}>
-        <img src={logo} style={{ height: "300px", width: '100%' }}></img>
+        <img src={logo} style={{ height: "55%", width: '90%' }}></img>
         <h1>Um sistema de recomendações de <span className={styles.cor}>dec
-        </span>isões de pr<span className={styles.cor}>o</span>jeto arquitetu<span className= {styles.cor}>ra</span>l</h1>
+        </span>isões de pr<span className={styles.cor}>o</span>jeto arquitetu<span className={styles.cor}>ra</span>l</h1>
       </dev>
       <dev className={styles.containerForm}>
         <h1>LOGIN</h1>
