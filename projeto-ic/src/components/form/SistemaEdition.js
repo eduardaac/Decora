afaa6035e2e88f543836dbb31adf070ef8dc94e7
Eduarda Araujo <duda2019h@gmail,com>
2023-09-08 16:52:32 -0300

    options não estão sendo salvas

--- projeto-ic/src/components/form/SistemaEdition.js
+++ projeto-ic/src/components/form/SistemaEdition.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { FaTrash, FaPlus } from "react-icons/fa"; // Importe os ícones
+import { FaTrash, FaPlus } from 'react-icons/fa';
 
 function SistemaEdition() {
   let { userId, codigoTurma } = useParams();
@@ -10,92 +10,152 @@ function SistemaEdition() {
   const [questions, setQuestions] = useState([]);
   const [newQuestion, setNewQuestion] = useState({
     label: '',
-    options: [],
+    options: [], // Agora options será um array de objetos com "text" e "answers"
     priority: 1,
     category: 'styles',
   });
   const [newOption, setNewOption] = useState('');
   const [newAnswer, setNewAnswer] = useState('');
-  const [addingAnswer, setAddingAnswer] = useState(false); // Estado para rastrear se estamos adicionando uma resposta
+  const [addingAnswer, setAddingAnswer] = useState(false);
+  const [previousState, setPreviousState] = useState(null);
+  const [isAddingOption, setIsAddingOption] = useState(false);
 
   useEffect(() => {
     if (codigoTurma) {
-      axios.get(`http://localhost:3333/questions/byclass/${codigoTurma}`)
-        .then(response => {
+      axios
+        .get(`http://localhost:3333/questions/byclass/${codigoTurma}`)
+        .then((response) => {
           setQuestions(response.data);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Erro ao buscar as perguntas:', error);
         });
     }
   }, [codigoTurma]);
 
   const handleDeleteQuestion = (questionId, indexToDelete) => {
-    axios.delete(`http://localhost:3333/users/${professorId}/questions/${questionId}`)
-      .then(response => {
-        setQuestions(prevQuestions =>
+    axios
+      .delete(`http://localhost:3333/users/${professorId}/questions/${questionId}`)
+      .then((response) => {
+        setQuestions((prevQuestions) =>
           prevQuestions.filter((_, index) => index !== indexToDelete)
         );
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Erro ao excluir a pergunta:', error);
       });
   };
 
   const handleAddOption = () => {
-    if (newOption.trim() !== '') {
-      if (addingAnswer) {
-        // Se estivermos adicionando uma resposta, vincule-a à opção atual
-        if (newAnswer.trim() !== '') {
-          setNewQuestion(prevQuestion => {
+    if (!isAddingOption) {
+      setIsAddingOption(true);
+      setNewOption('');
+      console.log('Iniciando a adição de uma nova opção.');
+    } else {
+      if (newOption.trim() !== '') {
+        if (addingAnswer) {
+          // Se estivermos adicionando uma resposta, vincule-a à opção atual
+          if (newAnswer.trim() !== '') {
+            setNewQuestion((prevQuestion) => {
+              const updatedOptions = [...prevQuestion.options];
+              // Adicione a nova resposta à opção atual
+              updatedOptions[updatedOptions.length - 1].answers.push(newAnswer);
+              console.log('Adicionando resposta à opção atual:', newAnswer);
+              return { ...prevQuestion, options: updatedOptions };
+            });
+          }
+          setAddingAnswer(false);
+          setNewAnswer('');
+          console.log('Finalizando a adição da resposta.');
+        } else {
+          // Salve o estado atual antes de começar a adição de uma resposta
+          setPreviousState({
+            newQuestion,
+            addingAnswer,
+            newOption,
+            newAnswer,
+          });
+
+          // Crie uma nova opção
+          const newOptionObject = {
+            text: newOption,
+            answers: [], // Inicialize as respostas como um array vazio
+          };
+
+          setNewQuestion((prevQuestion) => {
             const updatedOptions = [...prevQuestion.options];
-            updatedOptions[updatedOptions.length - 1].answers.push(newAnswer);
+            updatedOptions.push(newOptionObject);
+            console.log('Adicionando nova opção:', newOptionObject);
             return { ...prevQuestion, options: updatedOptions };
           });
+
+          setNewOption('');
+          setAddingAnswer(true);
         }
-        setAddingAnswer(false); // Finalize a adição da resposta
-        setNewAnswer(''); // Limpe o campo de resposta
-      } else {
-        // Se não estivermos adicionando uma resposta, crie uma nova opção
-        setNewQuestion(prevQuestion => ({
-          ...prevQuestion,
-          options: [...prevQuestion.options, { text: newOption, answers: [] }],
-        }));
-        setNewOption('');
-        setAddingAnswer(true); // Comece a adicionar uma resposta
       }
+      setIsAddingOption(false);
+      console.log('Finalizando a adição da opção.');
+      console.log('Novo estado da pergunta:', newQuestion);
     }
   };
 
+  const handleToggleAddingAnswer = () => {
+    if (addingAnswer) {
+      setAddingAnswer(false);
+      setNewAnswer('');
+      console.log('Finalizando a adição da resposta.');
+    } else {
+      setAddingAnswer(true);
+      console.log('Iniciando a adição de uma nova resposta.');
+    }
+  };
+
+  const handleFinishOption = () => {
+    setIsAddingOption(false);
+    setAddingAnswer(false);
+    setNewAnswer('');
+    setNewOption('');
+    console.log('Finalizando a adição da opção.');
+  };
+
   const handleSaveQuestion = () => {
-    // Verifique a validade da prioridade (1 a 5)
     if (newQuestion.priority < 1 || newQuestion.priority > 5) {
       alert('A prioridade deve estar entre 1 e 5.');
       return;
     }
 
-    // Verifique a validade da categoria (styles, decisions, technologies)
     const validCategories = ['styles', 'decisions', 'technologies'];
     if (!validCategories.includes(newQuestion.category)) {
       alert('A categoria deve ser "styles", "decisions" ou "technologies".');
       return;
     }
 
-    // Envie a nova pergunta para o servidor aqui
-    // Certifique-se de ajustar a lógica de envio de acordo com a estrutura esperada pelo servidor
-    console.log("Nova pergunta a ser enviada:", newQuestion);
-    // Aqui você pode fazer a solicitação POST para adicionar a pergunta
+    const newQuestionToSend = {
+      label: newQuestion.label,
+      priority: newQuestion.priority,
+      category: newQuestion.category,
+      options: newQuestion.options,
+    };
 
-    // Limpe o estado para preparar uma nova pergunta
-    setNewQuestion({
-      label: '',
-      options: [],
-      priority: 1,
-      category: 'styles',
-    });
-    setAddingAnswer(false);
-    setNewOption('');
-    setNewAnswer('');
+    console.log('Nova pergunta a ser enviada:', newQuestionToSend);
+
+    if (previousState) {
+      setNewQuestion(previousState.newQuestion);
+      setAddingAnswer(previousState.addingAnswer);
+      setNewOption(previousState.newOption);
+      setNewAnswer(previousState.newAnswer);
+      setPreviousState(null);
+    } else {
+      setNewQuestion({
+        label: '',
+        options: [],
+        priority: 1,
+        category: 'styles',
+      });
+      setAddingAnswer(false);
+      setNewOption('');
+      setNewAnswer('');
+    }
   };
 
   return (
@@ -104,15 +164,14 @@ function SistemaEdition() {
         {questions.map((question, index) => (
           <div className="formGroup" key={question.label}>
             <label>{question.label}</label>
-            <select
-              key={question.label}
-            >
+            <select key={question.label}>
               <option value="">Selecione opção...</option>
-              {question.options && question.options.map((option, optionIndex) => (
-                <option key={optionIndex} value={optionIndex}>
-                  {option.text}
-                </option>
-              ))}
+              {question.options &&
+                question.options.map((option, optionIndex) => (
+                  <option key={optionIndex} value={optionIndex}>
+                    {option.text}
+                  </option>
+                ))}
             </select>
             <button onClick={() => handleDeleteQuestion(question._id, index)}>
               <FaTrash />
@@ -132,14 +191,25 @@ function SistemaEdition() {
 
       <div className="formGroup">
         <label>Opções:</label>
-        <input
-          type="text"
-          value={newOption}
-          onChange={(e) => setNewOption(e.target.value)}
-        />
-        <button onClick={handleAddOption}>
-          {addingAnswer ? "Finalizar Opção" : <FaPlus />} 
-        </button>
+        {isAddingOption ? (
+          <>
+            <button onClick={handleToggleAddingAnswer}>
+              <FaPlus /> Adicionar Resposta
+            </button>
+            <button onClick={handleFinishOption}>Finalizar Opção</button>
+          </>
+        ) : (
+          <>
+            <input
+              type="text"
+              value={newOption}
+              onChange={(e) => setNewOption(e.target.value)}
+            />
+            <button onClick={handleAddOption}>
+              {addingAnswer ? 'Finalizar Opção' : <FaPlus />}
+            </button>
+          </>
+        )}
       </div>
 
       {addingAnswer && (
@@ -150,7 +220,7 @@ function SistemaEdition() {
             value={newAnswer}
             onChange={(e) => setNewAnswer(e.target.value)}
           />
-          <button onClick={handleAddOption}>
+          <button onClick={handleToggleAddingAnswer}>
             <FaPlus /> Adicionar Resposta
           </button>
         </div>
@@ -161,7 +231,9 @@ function SistemaEdition() {
         <input
           type="number"
           value={newQuestion.priority}
-          onChange={(e) => setNewQuestion({ ...newQuestion, priority: parseInt(e.target.value) })}
+          onChange={(e) =>
+            setNewQuestion({ ...newQuestion, priority: parseInt(e.target.value) })
+          }
         />
       </div>
 
