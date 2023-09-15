65e7c999a73cd458a60bee3b3a8dfa4246ffde24
Eduarda Araujo <duda2019h@gmail,com>
2023-09-15 20:53:55 -0300

    front de edition implementado

--- projeto-ic/src/components/form/SistemaEdition.js
+++ projeto-ic/src/components/form/SistemaEdition.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { FaTrash, FaPlus } from 'react-icons/fa';
+import { FaTrash } from 'react-icons/fa';
 
 const API_BASE_URL = "http://localhost:3333";
 
@@ -31,7 +31,6 @@ function SistemaEdition() {
 
   const handleDeleteQuestion = (questionId, indexToDelete) => {
     axios.delete(`${API_BASE_URL}/users/${professorId}/questions/${questionId}`)
-
       .then(response => {
         // Remova a questão da lista após a exclusão bem-sucedida
         setQuestions(prevQuestions =>
@@ -45,14 +44,16 @@ function SistemaEdition() {
 
   return (
     <div>
-      <ul>
-        {questions.map((question, index) => (
-          <div className="formGroup" key={question.label}>
+      {questions.map((question, index) => (
+        <div key={question.label} className="questionContainer">
+          <div className='deleteButtonContainer'>
+            <button className='deleteButton' onClick={() => handleDeleteQuestion(question._id, index)}>
+              <FaTrash />
+            </button>
+          </div>
+          <div className="formGroup">
             <label>{question.label}</label>
-            <select
-              key={question.label}
-
-            >
+            <select>
               <option value="">Selecione opção...</option>
               {question.options && question.options.map((option, optionIndex) => (
                 <option key={optionIndex} value={optionIndex}>
@@ -60,14 +61,9 @@ function SistemaEdition() {
                 </option>
               ))}
             </select>
-            
-            <button onClick={() => handleDeleteQuestion(question._id, index)}>
-              <FaTrash />
-            </button>
-    
           </div>
-        ))}
-      </ul>
+        </div>
+      ))}
     </div>
   );
 }
