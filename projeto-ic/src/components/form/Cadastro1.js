89b94d5d2f7dd87984a7a9e9f8267f89bdffc8a0
Eduarda Araujo <duda2019h@gmail,com>
2023-09-16 13:58:32 -0300

    Não permite um novo user por um email já existente

--- projeto-ic/src/components/form/Cadastro1.js
+++ projeto-ic/src/components/form/Cadastro1.js
@@ -1,53 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
-import { useForm } from "react-hook-form";
-import { isEmail } from "validator";
+import { useForm } from 'react-hook-form';
+import { isEmail } from 'validator';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { useState } from 'react';
 
-const API_BASE_URL = "http://localhost:3333";
+const API_BASE_URL = 'http://localhost:3333';
 
 const Cadastro1 = () => {
+    const navigate = useNavigate();
+
     const {
         register,
         handleSubmit,
         formState: { errors },
     } = useForm();
-    const [sucesso, setSucesso] = useState(false);
-    const navigate = useNavigate();
+
+    const [emailEmUso, setEmailEmUso] = useState(false);
+
+    const clearEmailEmUso = () => {
+        setEmailEmUso(false);
+    };
 
     const onSubmit = async (data) => {
-        setSucesso(true);
-        
         try {
             const response = await axios.post(`${API_BASE_URL}/users`, data);
-            const userId = response.data._id; // Supondo que a resposta contém o ID do usuário
-            
-            // Navegar para a próxima etapa, passando o userId
+            const userId = response.data._id;
+
             navigate('/Cadastro2', { state: { userId } });
         } catch (error) {
             console.log(error);
+            if (error.response && error.response.status === 400) {
+                setEmailEmUso(true);
+            }
         }
     };
-    
-
-    console.log("RENDER");
 
     return (
         <div className="form">
+
             <div className="formGroup">
                 <label>Nome</label>
                 <input
-                    className={errors?.nome && "input-error"}
-                    type="text" // Corrigido de "nome" para "text"
+                    className={errors?.nome && 'input-error'}
+                    type="text"
                     placeholder="Insira seu nome"
-                    {...register("nome", {
+                    {...register('nome', {
                         required: true,
                     })}
                 />
-
-                {errors?.nome?.type === "required" && (
+                {errors?.nome?.type === 'required' && (
                     <p className="error-message">O nome é necessário.</p>
                 )}
             </div>
@@ -55,20 +57,24 @@ const Cadastro1 = () => {
             <div className="formGroup">
                 <label>E-mail</label>
                 <input
-                    className={errors?.email && "input-error"}
+                    className={(errors?.email || emailEmUso) && 'input-error'} // Adicione uma classe de erro se houver um erro no email ou se o email estiver em uso
                     type="email"
                     placeholder="Insira seu e-mail"
-                    {...register("email", {
+                    {...register('email', {
                         required: true,
                         validate: (value) => isEmail(value),
                     })}
+                    onFocus={clearEmailEmUso} // Limpar a mensagem de erro ao focar no campo de email
                 />
-                {errors?.email?.type === "required" && (
+                {errors?.email?.type === 'required' && (
                     <p className="error-message">O e-mail é necessário.</p>
                 )}
 
-                {errors?.email?.type === "validate" && (
+                {errors?.email?.type === 'validate' && (
                     <p className="error-message">O e-mail é inválido.</p>
+                )}
+                {emailEmUso && (
+                    <p className="error-message">O e-mail já está em uso. Por favor, escolha outro.</p>
                 )}
             </div>
 
