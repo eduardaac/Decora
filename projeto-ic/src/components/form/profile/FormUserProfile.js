385810caf0349444adfb47956ab9a617fa6c4488
Eduarda Araujo <duda2019h@gmail,com>
2023-11-01 13:01:00 -0300

    data de nascimento com a formatação normal

--- projeto-ic/src/components/form/profile/FormUserProfile.js
+++ projeto-ic/src/components/form/profile/FormUserProfile.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
-import '../Form.css'
+import '../Form.css';
 
 const FormUserProfile = () => {
   const { userId } = useParams();
@@ -11,16 +11,28 @@ const FormUserProfile = () => {
 
   useEffect(() => {
     if (userId) {
-      axios.get(`https://decora-back.vercel.app/users/${userId}`)
-        .then(response => {
+      axios
+        .get(`https://decora-back.vercel.app/users/${userId}`)
+        .then((response) => {
           setUserData(response.data);
           console.log(response.data);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Erro ao obter dados do usuário:', error);
         });
     }
   }, [userId]);
+
+  function formatDataNascimento(dataNascimento) {
+    if (!dataNascimento) return '';
+
+    const date = new Date(dataNascimento);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  }
 
   return (
     <div className='form'>
@@ -32,7 +44,12 @@ const FormUserProfile = () => {
         <input type='email' id='email' value={userData.email} readOnly />
 
         <label htmlFor='dataNascimento'>Data de Nascimento:</label>
-        <input type='text' id='dataNascimento' value={userData.dataNascimento} readOnly />
+        <input
+          type='text'
+          id='dataNascimento'
+          value={formatDataNascimento(userData.dataNascimento)}
+          readOnly
+        />
 
         <label htmlFor='atuacao'>Atuação:</label>
         <input type='text' id='atuacao' value={userData.atuacao} readOnly />
