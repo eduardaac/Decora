f958606120c359dbfaf2968dfb1d12ce8d64eaec
Eduarda Araujo <duda2019h@gmail,com>
2023-11-01 10:57:17 -0300

    configurando arquivo vercel

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,6 @@ const app = express();
 const routes = require('./routes');
 require('./config/dbConfig');
 
-// Configurar o CORS para permitir solicitações da origem do aplicativo React
 app.use(cors({
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
